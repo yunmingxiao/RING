4d09b3283dc260331b67f5c35895d2015e4b0396
Yunming Xiao <xyunming@live.com>
2020-12-31 22:05:09 -0600

    fix default policy

--- scripts/policy/mysterium_default.py
+++ scripts/policy/mysterium_default.py
@@ -19,9 +19,9 @@ def bandwidth_policy(now, month_start, next_month,
     next_month : float
         The timestamp at the beginning of next month (in seconds).
     data_plan : float
-        The data cap for the current month (in Gigabytes).
+        The data cap for the current month (in bytes).
     used_data : float
-        The data have used in the current month (in Gigabytes).
+        The data have used in the current month (in bytes).
     curr_bandwidth_limit : float
         The current bandwidth limit (in Mbps)
     bandwidth_history : list [{'time': float, 'Bps': float}]
@@ -29,11 +29,12 @@ def bandwidth_policy(now, month_start, next_month,
 
     """
     left_data = (data_plan - used_data) * 8 # bytes to bits
-    time_left = next_month - month_start
+    time_left = next_month - now
     new_ratio = curr_bandwidth_limit \
                 / (60*8*sum([bh['Bps'] for bh in bandwidth_history])) \
                 / (len(bandwidth_history)*60)
 
+    global bwlimit_time_ratio
     if bwlimit_time_ratio <= 0:
         # initialized the bandwidth-time-ratio 
         bwlimit_time_ratio = new_ratio
