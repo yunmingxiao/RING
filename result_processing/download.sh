9c3d570cb3b4f7dc1ecaf5f90e3964ae2f86cef1
Yunming Xiao <xyunming@live.com>
2021-07-08 09:49:06 -0500

    update

--- result_processing/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-curl http://165.124.180.66:45678/mcafee_category.json > sample/mcafee_category.json
-curl http://165.124.180.66:45678/mcafee_security.json > sample/mcafee_security.json
--- result_processing/update_gsb.py
+++
@@ -1,5 +0,0 @@
-from gglsbl import SafeBrowsingList
-import Constants #dvpn.result_processing.Constants
-
-sbl = SafeBrowsingList(Constants.gsbk, 'gsb_v4.db')
-sbl.update_hash_prefix_cache()
